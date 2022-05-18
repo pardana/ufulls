b18f3fea0777961e698b0a44cf794b06f4f2fc50
Aditria Pardana <aditria.pardana@yahoo.co.id>
2022-05-18 11:25:38 +0700

    Template string ES6

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-if(true){
-    let name = "Aditria";
-    name = "Putra";
-    alert(name);
-}
+let fname = 'Aditria';
+let lname = 'Putra';
+let age = prompt("Guest Aditria's age...");
+
+//old way
+// let result = fname + ' ' + lname + ' is ' + age + ' years old';
+// alert(result)
+
+// using template string
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result)
