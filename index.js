23d2784a8e5da9228c4904411e28b8791371d343
Aditria Pardana <aditria.pardana20@gmail.com>
2022-05-30 11:05:23 +0700

    Destructuring Array

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-let uniStudent = ({name, university}) => {
-    
-    console.log(`${name} from ${university}`);
-};
-
-uniStudent({
-    name: 'Aditria',
-    university: 'STMIK Bani Saleh'
-});
+let [, , mountain] = ['Slamet', 'Ranca Upas', 'Bromo'];
+console.log(mountain)
