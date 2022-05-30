13cc7a1aff40cbc65486b216978adba93ca89bfb
Aditria Pardana <aditria.pardana20@gmail.com>
2022-05-30 11:41:47 +0700

    class constructor super

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var rivers = ['sunkoshi', 'tyamakoshi', 'saptakoshi'];
-var [first, ...last] = rivers;
+//parent class
+class Holiday {
+    constructor(destination, days){
+        this.destination = destination,
+        this.days = days
+    }
 
-console.log(last)
+    info(){
+        console.log(`${this.destination} will take ${this.days} days.`);
+    }
+}
+
+//sub class or child
+class Expedition extends Holiday{
+    constructor(destination, days, gear){
+        super(destination, days);
+        this.gear = gear
+    }
+
+    info(){
+        super.info();
+        console.log(`Bring your ${this.gear.join(" - ")}`)
+    }
+}
+
+const tripWithGear = new Expedition("Nepal", 30, ['Sunglasses', 'Flags', 'Camera']);
+tripWithGear.info();
